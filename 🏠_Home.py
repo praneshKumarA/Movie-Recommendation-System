8477429d6fe9a65a190315cd37f62b082be2b844
Pranesh Kumar A <53888235+praneshKumarA@users.noreply.github.com>
2023-04-04 16:37:15 +0530

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(page_title = 'Movie Recommendation System', page_icon = '🎥')
-st.markdown('# Movie Recommendation System')
+st.markdown("<h1 style='text-align: center; color: grey; font-family: Courier;'>Movie Recommendation System</h1>", unsafe_allow_html=True)
+for _ in range(5):
+    st.write(' ')
+image = Image.open('movie-recommendation.jpg')
+st.image(image, use_column_width = True)

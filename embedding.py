011bfd9f8dd75a4e7e497ddcd48d0612262490c4
Pranesh Kumar A <53888235+praneshKumarA@users.noreply.github.com>
2023-04-04 16:35:54 +0530

    Update embedding.py

--- embedding.py
+++ embedding.py
@@ -1,7 +1,5 @@
 import pinecone
-import string
 from tqdm.auto import tqdm
-import pandas as pd
 
 import utils
 from utils import return_df
@@ -35,18 +33,11 @@ def upsert_vectors(index):
         i_end = min(i+batch_size, len(df))
         batch = df.iloc[i:i_end]
         emb = utils.return_retriever().encode(batch["Description_punc_removed"].tolist()).tolist()
-        # print(batch['text'].tolist())
-        # print('batch size:', len(batch))
         batch_text = batch['Description'].tolist()
-        # non_zero = [i for i in range(len(batch_text)) if len(batch_text[i])==0]
-        # print(non_zero)
         entities = utils.extract_named_entities(batch_text)
-        # print(entities)
         batch["named_entities"] = [list(set(entity)) for entity in entities]
-        # batch = batch.drop(['Certificate', 'Duration', 'Genre', 'Rate', 'Metascore', 'Cast' ,'Info'], axis=1)
         batch = batch.drop(['type', 'director', 'cast', 'country', 'date_added' ,'release_year', 'rating', 'duration', 'listed_in'], axis=1)
         meta = batch.to_dict(orient="records")
         ids = [f"{idx}" for idx in range(i, i_end)]
         to_upsert = list(zip(ids, emb, meta))
         _ = index.upsert(vectors=to_upsert)
-        # print('end of iteration')

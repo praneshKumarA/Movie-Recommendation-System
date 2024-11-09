6079485e8b1f93f35efb4fc5b098db0c11fba946
Pranesh Kumar A <153798119+pranesh-apiplatform@users.noreply.github.com>
2024-11-09 19:24:59 +0530

    Revert "Update embedding.py (9/11/24)"
    This reverts commit cc0ac846591bd6eed2b4eb9677fff6d08a6de5e2.

--- embedding.py
+++ embedding.py
@@ -5,11 +5,10 @@ import utils
 from utils import return_df
 
 def pinecone_index():
-    pinecone = Pinecone(api_key="ec1a3181-1a35-4441-a871-c06df4567b0d")
-    # pinecone.init(
-    #     api_key="ec1a3181-1a35-4441-a871-c06df4567b0d",
-    #     environment="eu-west1-gcp"
-    # )
+    pinecone.init(
+        api_key="ec1a3181-1a35-4441-a871-c06df4567b0d",
+        environment="eu-west1-gcp"
+    )
 
 
     return pinecone
